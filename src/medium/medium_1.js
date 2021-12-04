2fb64530d5545d1676460f9781440ee6f5939e03
Lidia Mohammed <lidiamohammed@Lidias-MBP.lan>
2021-12-04 17:53:47 -0500

    a

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,7 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    Array.sort(array);
+    array.sort();
 
     var median = 0;
     if (array.length % 2 == 0) {
