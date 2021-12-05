35044d16bf49e149d8a2fbe048441df0024d96c6
Lidia Mohammed <lidiamohammed@Lidias-MBP.lan>
2021-12-04 23:01:47 -0500

    f

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -29,7 +29,9 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-
+    for (let i = 10; i < 0; i--) {
+        console.log("Hello, World!")
+    }
 };
 
 
@@ -46,7 +48,8 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    let num = num1 * num2;
+    return num;
 };
 
 
@@ -54,7 +57,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -62,7 +65,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50)
 };
 
 
